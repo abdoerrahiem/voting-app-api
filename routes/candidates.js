2ee0a03d789ad6e3d6b977f7e7303f4fb62937ab
Abdur Rahim <abdoerrahiem@gmail.com>
2020-09-29 14:25:41 +0700

    edit candidates route

--- routes/candidates.js
+++ routes/candidates.js
@@ -12,13 +12,12 @@ const auth = require('../middleware/auth')
 const admin = require('../middleware/admin')
 
 router.route('/').post(auth, admin, createCandidate).get(getCandidates)
-router.use(auth)
 router
   .route('/:id')
   .get(getCandidate)
-  .delete(admin, deleteCandidate)
-  .put(admin, updateCandidate)
-router.put('/:id/chairman/photo', admin, addPhotoChairman)
-router.put('/:id/viceChairman/photo', admin, addPhotoViceChairman)
+  .delete(auth, admin, deleteCandidate)
+  .put(auth, admin, updateCandidate)
+router.put('/:id/chairman/photo', auth, admin, addPhotoChairman)
+router.put('/:id/viceChairman/photo', auth, admin, addPhotoViceChairman)
 
 module.exports = router
